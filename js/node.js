234a77f51f5d7995a6dcb81537c58969aa282fe1
Kaplan75 <96084273+Kaplan75@users.noreply.github.com>
2023-07-23 21:44:22 +0300

    Add files via upload

---
+++ js/node.js
@@ -0,0 +1,4 @@
+
+const loveButton = document.getElementById('loveButton'); 
+const loveMessage = document.querySelector('.loveMessage'); 
+loveButton.addEventListener('click', () => { loveMessage.textContent = 'А теперь иди нахуй )'; });
